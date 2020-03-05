c1f2084449d48fb3f9e010e610bef3e5830e9af0
M3RLN <32886755+M3RLN@users.noreply.github.com>
2020-03-04 21:47:28 -0800

    Initial Commit

---
+++ Assets/JumpScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    public Rigidbody2D rb2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2.AddRelativeForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
+        if (gameObject.transform.position.y <= 0)
+        {
+            gameObject.transform.rotation = Quaternion.identity;
+            rb2.freezeRotation = true;
+            rb2.velocity = new Vector2(Input.GetAxis("Horizontal") * 3, rb2.velocity.y);
+        }
+        else
+        {
+            rb2.freezeRotation = false;
+            gameObject.transform.Rotate(0,0,Input.GetAxis("Horizontal"));
+        }
+        
+    }
+}
