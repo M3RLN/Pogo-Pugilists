9fec9dde4e302c372b323c9fb0c8cc5f7f8c3415
Malcolm Morales <malcolma.morales1998@gmail.com>
2020-03-16 21:39:10 -0700

    Players are now instances of a prefab. Each has their own input.

--- Pogo-Pugilists-master/Assets/JumpScript.cs
+++ Pogo-Pugilists-master/Assets/JumpScript.cs
@@ -7,40 +7,43 @@ public class JumpScript : MonoBehaviour
     Rigidbody2D rb2;
     public GameObject HitBox;
     GameObject newHitBox;
-   
+    public string moveAxis;
+    public string jumpButton;
+
     //public float lockPos;
-   
+
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb2 = GetComponent<Rigidbody2D>();
         //lockPos = 0;
-  
 
-        
+
+
     }
     //ghuhiu
     // Update is called once per frame
     void Update()
     {
-      
-        
-        if (Input.GetButtonDown("Jump"))
+
+
+        if (Input.GetButtonDown(jumpButton))
         {
             rb2.AddRelativeForce(Vector2.up * 10, ForceMode2D.Impulse);
-            newHitBox = Instantiate(HitBox, new Vector3(transform.position.x, transform.position.y - 1.5f, 0), transform.rotation);
+            //newHitBox = Instantiate(HitBox, new Vector3(transform.position.x, transform.position.y - 1.5f, 0), transform.rotation);
+            newHitBox = Instantiate(HitBox, gameObject.transform.position + Vector3.down, transform.rotation);
             newHitBox.GetComponent<PushBackScript>().Force = rb2;
         }
-        
+
         else
         {
             rb2.freezeRotation = false;
             //gameObject.transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
-            gameObject.transform.Rotate(0,0,Input.GetAxis("Horizontal"));
+            gameObject.transform.Rotate(0,0,Input.GetAxis(moveAxis));
         }
-        
+
     }
 
-   
+
 }
