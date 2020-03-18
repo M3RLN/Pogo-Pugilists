d061882cd1b5757a64619747d66278513cd96412
IvanAKim <Ivanalejandrokim@gmail.com>
2020-03-17 23:59:18 -0700

    Update Kinda Fixed the Restart Button

--- Pogo-Pugilists-master/Assets/SelectionScript.cs
+++ Pogo-Pugilists-master/Assets/SelectionScript.cs
@@ -10,6 +10,7 @@ public class SelectionScript : MonoBehaviour
     public bool p2;
     public Text selectionText;
     public StaticItemSelection sis;
+    public GameObject myOb;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,7 @@ public class SelectionScript : MonoBehaviour
     {
         if(!p1 && !p2)
         {
+            DontDestroyOnLoad(myOb);
             SceneManager.LoadScene("SampleScene");
         }
         if (p1)
